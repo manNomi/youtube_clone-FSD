ebe69a26d0bbf2015d3e6c07cc324c84fcdd5dd1
Ubuntu <ubuntu@ip-172-31-14-192.ap-northeast-2.compute.internal>
2024-08-17 09:12:22 +0000

    feat:이미지 주소 수정

--- ROOT/src/entities/video_img/ui/video_img.js
+++ ROOT/src/entities/video_img/ui/video_img.js
@@ -1,5 +1,7 @@
+import css from "./video_img.module.css";
 export const makeThumbImg = (thumb_img) => {
   const new_mainImg = document.createElement("img");
   new_mainImg.setAttribute("src", thumb_img);
+  new_mainImg.className = css.root;
   return new_mainImg;
 };
--- ROOT/src/pages/youtube/youtubeData.js
+++ ROOT/src/pages/youtube/youtubeData.js
@@ -4,7 +4,7 @@ export const getData = () => {
   const Video = {
     title:
       "𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭 돌아가고 싶은 그때 그 시절 2010년 감성힙합 I 다이나믹듀오, 프라이머리, 긱스, 개리, 빈지노",
-    thumbImg: __dirname + "../public/image/main/main.play_img.PNG",
+    thumbImg: "https://img.youtube.com/vi/iFs8AfODM74/mqdefault.jpg",
     userImg: __dirname + "../public/image/main/main.playname_img.PNG",
     userName: "올끌 (All of MBClassic)",
     thumbView: 441231,
